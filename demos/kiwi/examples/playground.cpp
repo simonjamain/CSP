b4ae7d806af90daa846b218909e12b76b6beafc8
simon jamain <jamain.simon@gmail.com>
2015-06-26 11:28:03 +0200

    - mise a jour du playground kiwi, et que même que y'a po les stay constraints T-T ouin ouin

--- demos/kiwi/examples/playground.cpp
+++ demos/kiwi/examples/playground.cpp
@@ -1,6 +1,30 @@
 #include <kiwi.h>
 
+using namespace kiwi;
+
 int main( int argc, const char* argv[] )
 {
-    std::cout << "test\n";
+    Solver solver{};
+
+    Variable t1("t1");
+    Variable t2("t2");
+    Variable min1("min1");
+    Variable max1("max1");
+    Variable nom1("nom1");
+
+    solver.addEditVariable(t1, strength::strong);
+    solver.addEditVariable(t2, strength::strong);
+    solver.addEditVariable(min1, strength::required);
+    solver.addEditVariable(max1, strength::required);
+    solver.addEditVariable(nom1, strength::medium);
+
+    solver.addConstraint(min1 >= 0);
+    solver.addConstraint(min1 <= nom1);
+    solver.addConstraint(nom1 <= max1);
+    solver.addConstraint(t2 == t1 + nom1);
+
+    solver.dump();
+
+    return 0;
 }
+
